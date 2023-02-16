dec1a50f2e62ecfd5341a6453ab07c88cf54053e
jeremyloh-hub <eziorain@gmail.com>
2023-02-16 21:55:39 +0800

    Created Object for the game

---
+++ main.js
@@ -0,0 +1,10 @@
+const game = { 
+    player: {
+        lifepoints: 0,
+        points:     0,
+        highscore:  0,
+        abilities:  {remove1Pair: false, revealAll: false, addLp: false, addTime: false}
+    },
+    timer:          0,
+    cards: ['','','','','','','','','','','',''],
+}
