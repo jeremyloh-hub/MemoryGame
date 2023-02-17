960b1437b73f8f6b603894006c02fe29882ad7b5
jeremyloh-hub <eziorain@gmail.com>
2023-02-17 11:07:35 +0800

    added cardcover & render display
    added new image for card cover,
    render display like points, timer & lifepoints

--- main.js
+++ main.js
@@ -1,40 +1,80 @@
 const game = { 
     player: {
-        lifepoints: 0,
-        points:     0,
+        lifepoints: 10,
+        points:     2,
         highscore:  0,
         abilities:  {remove1Pair: false, revealAll: false, addLp: false, addTime: false}
     },
-    timer:          0,
-    cards: ['Assets/1.jpeg',
-            'Assets/1.jpeg',
-            'Assets/2.jpeg',
-            'Assets/2.jpeg',
-            'Assets/3.webp',
-            'Assets/3.webp',
-            'Assets/4.webp',
-            'Assets/4.webp',
-            'Assets/5.jpeg',
-            'Assets/5.jpeg',
-            'Assets/6.webp',
-            'Assets/6.webp'],
+    timer:          120,
+    cardShown: ['Assets/1.jpeg',
+                'Assets/1.jpeg',
+                'Assets/2.jpeg',
+                'Assets/2.jpeg',
+                'Assets/3.webp',
+                'Assets/3.webp',
+                'Assets/4.webp',
+                'Assets/4.webp',
+                'Assets/5.jpeg',
+                'Assets/5.jpeg',
+                'Assets/6.webp',
+                'Assets/6.webp'],
+
+    cards:     ['Assets/cardcover.webp',
+                'Assets/cardcover.webp',
+                'Assets/cardcover.webp',
+                'Assets/cardcover.webp',
+                'Assets/cardcover.webp',
+                'Assets/cardcover.webp',
+                'Assets/cardcover.webp',
+                'Assets/cardcover.webp',
+                'Assets/cardcover.webp',
+                'Assets/cardcover.webp',
+                'Assets/cardcover.webp',
+                'Assets/cardcover.webp']
+
 }
 
 
-function renderCards(){
+function renderCards(card){
     const selectCardDiv = document.querySelector('.cards')
     selectCardDiv.textContent = '';
 
-    for (let i = 0; i < game.cards.length; i++) {
-        const cardArray = game.cards[i];
+    for (let i = 0; i < card.length; i++) {
+        const cardArray = card[i];
         const createCards = document.createElement('img');
 
         createCards.setAttribute('id',i);
         createCards.setAttribute('src',cardArray);
         selectCardDiv.appendChild(createCards);
-
     }
+}
+
+function renderDisplay(){
+    const selectDisplayDiv = document.querySelector('.display');
+    selectDisplayDiv.textContent = '';
+
+    const points = game.player.points;
+    const timer = game.timer;
+    const lp = game.player.lifepoints;
+    const createPoints = document.createElement('h2');
+    const createTimer = document.createElement('h2');
+    const createLp = document.createElement('h2');
+
+    createPoints.setAttribute('id','points');
+    createTimer.setAttribute('id','timer');
+    createLp.setAttribute('id','lp');
+
+    createPoints.innerText = `Points: ` + points;
+    createTimer.innerText = `Timer: ` + timer;
+    createLp.innerText = `LifePoints: ` + lp;
+
+    selectDisplayDiv.appendChild(createPoints);
+    selectDisplayDiv.appendChild(createTimer);
+    selectDisplayDiv.appendChild(createLp);
 
 }
 
-renderCards();
+
+
+renderDisplay();
+renderCards(game.cards);
